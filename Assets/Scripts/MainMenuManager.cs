4f25008ed08a703dccc9aabee6b0f06aecd29d40
lucasbrun196 <luucaasbrum13@gmail.com>
2025-11-10 22:29:59 -0300

    menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private GameObject initialMenu;
+    [SerializeField] private GameObject difficultyMenu;
+    public void Play()
+    {
+        Debug.Log("OLAAA");
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void OpenDifficultyMenu()
+    {
+        difficultyMenu.SetActive(true);
+        initialMenu.SetActive(false);
+    }
+    
+    public void CloseDifficultyMenu()
+    {
+        initialMenu.SetActive(true);
+        difficultyMenu.SetActive(false);
+    }
+
+}
