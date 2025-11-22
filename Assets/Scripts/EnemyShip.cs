8ddaaf2096f2566401baf504a63ba7de1fbbc11b
manupicoli <manuelaantunes004@gmail.com>
2025-11-22 01:17:18 -0300

    add collider for enemy ship

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -3,9 +3,24 @@ using UnityEngine;
 public class EnemyShip : MonoBehaviour
 {
     public float speed = 3f;
+    private Rigidbody2D rb;
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
+    }
 
     void Update()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        rb.linearVelocity = Vector2.left * speed;
     }
+
+    public void StopAndPushBack(Vector2 pushDirection, float pushForce)
+    {
+        speed = 0;
+        rb.linearVelocity = Vector2.zero;
+        rb.AddForce(pushDirection * pushForce, ForceMode2D.Impulse);
+    }
+
 }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -11,10 +11,32 @@ public class PlayerCollision : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Asteroid") || collision.CompareTag("EnemyShip"))
+        if (collision.CompareTag("Asteroid"))
         {
             manager.GameOver();
+
+            GetComponent<MainShipMove>().enabled = false;
+            GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+
             gameObject.SetActive(false);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyShip"))
+        {
+            manager.GameOver();
+
+            GetComponent<MainShipMove>().enabled = false;
+            GetComponent<Rigidbody2D>().linearVelocity = Vector2.zero;
+
+            var enemy = collision.gameObject.GetComponent<EnemyShip>();
+            if (enemy != null)
+            {
+                Vector2 pushDir = (enemy.transform.position - transform.position).normalized;
+                enemy.StopAndPushBack(pushDir, 5f);
+            }
         }
     }
 }
