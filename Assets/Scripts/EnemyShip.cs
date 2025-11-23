87c914aa58aec84fe7eebe7ed6c62766e521fbe3
manupicoli <manuelaantunes004@gmail.com>
2025-11-23 00:57:24 -0300

    improve enemy ship movement

--- Assets/Scripts/EnemyShip.cs
+++ Assets/Scripts/EnemyShip.cs
@@ -4,8 +4,9 @@ public class EnemyShip : MonoBehaviour
 {
     public float speed = 3f;
     private Rigidbody2D rb;
+    private Vector2 direction = Vector2.left;
 
-     void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
@@ -13,7 +14,7 @@ public class EnemyShip : MonoBehaviour
 
     void Update()
     {
-        rb.linearVelocity = Vector2.left * speed;
+        rb.linearVelocity = direction * speed;
     }
 
     public void StopAndPushBack(Vector2 pushDirection, float pushForce)
@@ -21,6 +22,21 @@ public class EnemyShip : MonoBehaviour
         speed = 0;
         rb.linearVelocity = Vector2.zero;
         rb.AddForce(pushDirection * pushForce, ForceMode2D.Impulse);
+    }
+
+    public void ReverseDirection()
+    {
+        direction *= -1;
+
+        transform.Rotate(0f, 0, 180f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Border"))
+        {
+            ReverseDirection();
+        }
     }
 
 }
